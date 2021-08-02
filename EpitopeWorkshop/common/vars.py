1f838d3ebde2cda792044464a8179647561a100c
Sid Feiner <sfeiner@vmware.com>
2021-08-03 01:14:14 +0300

    use python 3.7, use linear db and handle new letters representing multiple possible amino acids

---
+++ EpitopeWorkshop/common/vars.py
@@ -0,0 +1,7 @@
+from Bio.SeqUtils import ProtParamData
+
+AMINO_ACIDS = ProtParamData.kd.keys()
+
+# Polarity based on: https://teaching.ncl.ac.uk/bms/wiki/index.php/Amino_acids
+POLAR_AMINO_ACIDS = ['R', 'N', 'D', 'C', 'Q', 'E', 'H', 'K', 'S', 'T', 'Y']
+AMINO_ACIDS_POLARITY_MAPPING = {aa: aa in POLAR_AMINO_ACIDS for aa in AMINO_ACIDS}
--- EpitopeWorkshop/process/features.py
+++ EpitopeWorkshop/process/features.py
@@ -3,13 +3,30 @@ from Bio.SeqUtils import ProtParamData
 from Bio.SeqUtils.ProtParam import ProteinAnalysis
 from memoization import cached
 from quantiprot.metrics.aaindex import get_aa2volume
+
+from EpitopeWorkshop.common import vars
 from EpitopeWorkshop.common.contract import *
+
+GROUPED_AA = {
+    'X': None,
+    'B': ['D', 'N'],
+    'Z': ['E', 'Q'],
+    'J': ['L', 'I']
+}
+
+
+def add_group_type_values(d: dict) -> dict:
+    new_dict = {k: float(v) for k, v in d.items()}
+    for group, aas in GROUPED_AA.items():
+        keys = aas if aas is not None else list(d.keys())
+        new_dict[group] = sum([float(d[item]) for item in keys]) / len(keys)
+    return new_dict
 
 
 class FeatureCalculator:
-    # Polarity based on: https://teaching.ncl.ac.uk/bms/wiki/index.php/Amino_acids
-    POLAR_AAS = ['R', 'N', 'D', 'C', 'Q', 'E', 'H', 'K', 'S', 'T', 'Y']
-    AA_TO_VOLUME_MAPPING = get_aa2volume()
+    AA_TO_VOLUME_MAPPING = add_group_type_values(get_aa2volume().mapping)
+    AA_TO_HYDROPHPBICITY_MAPPING = add_group_type_values(ProtParamData.kd)
+    AA_TO_POLARITY_MAPPING = add_group_type_values(vars.AMINO_ACIDS_POLARITY_MAPPING)
 
     def _key_amino_acid(self, row):
         return row[TYPE_COL_NAME]
@@ -26,20 +43,19 @@ class FeatureCalculator:
 
     @cached(custom_key_maker=_key_amino_acid)
     def _calculate_computed_volume(self, row: pd.Series):
-        return self.AA_TO_VOLUME_MAPPING(row[TYPE_COL_NAME].upper())
+        return self.AA_TO_VOLUME_MAPPING[row[TYPE_COL_NAME].upper()]
 
     @cached(custom_key_maker=_key_amino_acid)
     def _calculate_hydrophobicity(self, row: pd.Series):
-        return ProtParamData.kd[row[TYPE_COL_NAME]]
+        return self.AA_TO_HYDROPHPBICITY_MAPPING[row[TYPE_COL_NAME]]
 
     @cached(custom_key_maker=_key_amino_acid)
     def _calculate_polarity(self, row: pd.Series):
-        return row[TYPE_COL_NAME] in self.POLAR_AAS
+        return self.AA_TO_POLARITY_MAPPING[row[TYPE_COL_NAME]]
 
-    @cached(custom_key_maker=_key_seq_id_amino)
+    @cached(custom_key_maker=_key_amino_acid)
     def _calculate_relative_surface_accessibility(self, row: pd.Series):
-        seq, amino_acid_index = row[SEQ_COL_NAME], row[AMINO_ACID_INDEX_COL_NAME]
-        return None
+        return ProtParamData.em[row[TYPE_COL_NAME]]
 
     @cached(custom_key_maker=_key_seq_id_amino)
     def _calculate_secondary_surface(self, row: pd.Series):
@@ -49,7 +65,7 @@ class FeatureCalculator:
         return row[SEQ_COL_NAME][row[AMINO_ACID_INDEX_COL_NAME]].upper()
 
     def calculate_features(self, df: pd.DataFrame) -> pd.DataFrame:
-        df[ANALYZED_SEQ_COL_NAME] = df.apply(self._calculate_analyzed_sequence, axis=1)
+        # df[ANALYZED_SEQ_COL_NAME] = df.apply(self._calculate_analyzed_sequence, axis=1)
 
         # Type must be calculated first because it is used in next steps
         df[TYPE_COL_NAME] = df.apply(self._calculate_type, axis=1)
@@ -57,6 +73,6 @@ class FeatureCalculator:
         df[HYDROPHOBICITY_COL_NAME] = df.apply(self._calculate_hydrophobicity, axis=1)
         df[COMPUTED_VOLUME_COL_NAME] = df.apply(self._calculate_computed_volume, axis=1)
         df[POLARITY_COL_NAME] = df.apply(self._calculate_polarity, axis=1)
-        df[SS_COL_NAME] = df.apply(self._calculate_secondary_surface, axis=1)
+        # df[SS_COL_NAME] = df.apply(self._calculate_secondary_surface, axis=1)
         df[RSA_COL_NAME] = df.apply(self._calculate_relative_surface_accessibility, axis=1)
         return df
