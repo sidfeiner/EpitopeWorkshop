5167252870c0aa2f9135586a39c01a09b898deec
Sid Feiner <sfeiner@vmware.com>
2021-08-18 22:15:19 +0300

    find pdbs with unknown chains

--- EpitopeWorkshop/process/read.py
+++ EpitopeWorkshop/process/read.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import List, Optional, Set
 
 from Bio import SeqIO, Seq
 import pandas as pd
@@ -8,10 +8,18 @@ from EpitopeWorkshop.common.contract import *
 from EpitopeWorkshop.common.conf import DEFAULT_WINDOW_SIZE
 
 
-def read_fasta(path: str, with_sliding_window: bool = True,
-               sliding_window_size: int = DEFAULT_WINDOW_SIZE,
-               limit_sequences_amt: Optional[int] = None
-               ) -> pd.DataFrame:
+def load_sequences_from_fasta(path: str) -> Set[str]:
+    s = set()
+    with open(path) as handle:
+        for idx, record in enumerate(SeqIO.parse(handle, "fasta")):
+            s.add(record.seq)
+    return s
+
+
+def load_fasta_fasta(path: str, with_sliding_window: bool = True,
+                     sliding_window_size: int = DEFAULT_WINDOW_SIZE,
+                     limit_sequences_amt: Optional[int] = None
+                     ) -> pd.DataFrame:
     ids = []
     seqs = []
     sub_seqs = []
@@ -20,7 +28,8 @@ def read_fasta(path: str, with_sliding_window: bool = True,
     is_in_epitope = []
     with open(path) as handle:
         for idx, record in enumerate(SeqIO.parse(handle, "fasta")):
-            current_sub_seqs = utils.split_to_subsequences(record.seq, sliding_window_size) if with_sliding_window else [
+            current_sub_seqs = utils.split_to_subsequences(record.seq,
+                                                           sliding_window_size) if with_sliding_window else [
                 record.seq]
             if with_sliding_window:
                 assert len(current_sub_seqs) == (len(record.seq) - sliding_window_size + 1)
@@ -31,7 +40,8 @@ def read_fasta(path: str, with_sliding_window: bool = True,
             sub_seqs.extend((sub_seq for current_sub_seq in current_sub_seqs for sub_seq in
                              [current_sub_seq] * mult_subseq_values))
             amino_acid_index_in_seq.extend(
-                (sub_seq_ix + index for sub_seq_ix, sub_seq in enumerate(current_sub_seqs) for index in range(len(sub_seq))))
+                (sub_seq_ix + index for sub_seq_ix, sub_seq in enumerate(current_sub_seqs) for index in
+                 range(len(sub_seq))))
             amino_acid_index_in_subseq.extend((index for sub_seq in current_sub_seqs for index in range(len(sub_seq))))
             is_in_epitope.extend((aa.isupper() for sub_seq in current_sub_seqs for aa in sub_seq))
             if len(record.features):
---
+++ EpitopeWorkshop/scripts/find_unknown_peptides.py
@@ -0,0 +1,34 @@
+import os
+from glob import glob
+from typing import List, Set, Optional
+
+import fire
+from Bio import SeqIO
+from Bio.PDB import PDBParser
+
+from EpitopeWorkshop.common import contract
+from EpitopeWorkshop.process.read import load_sequences_from_fasta
+
+
+class FindUnknownPeptides:
+    def load_fasta_file(self, path: str) -> Set[str]:
+        sequences = load_sequences_from_fasta(path)
+        peptides_only = {item.lower() for item in sequences}
+        return set(peptides_only)
+
+    def find(self, known_peptides_fasta_file: str, unknown_structures_dir_path: str, limit: Optional[int] = None):
+        known_peptides = self.load_fasta_file(known_peptides_fasta_file)
+        pdb_files_wildcard = os.path.join(unknown_structures_dir_path, '*.pdb')
+        pdb_files = glob(pdb_files_wildcard)
+        for pdb_file_path in pdb_files:
+            print(f"searching pdb file {pdb_file_path}")
+            with open(pdb_file_path, 'r') as pdb_file:
+                for record in SeqIO.parse(pdb_file, 'pdb-atom'):
+                    if record.seq.lower() in known_peptides:
+                        print(f"!!!!!!known chain found in {pdb_file_path}")
+                    else:
+                        print(f"unknown chain found in {pdb_file_path}: {record.seq.lower()}")
+
+
+if __name__ == '__main__':
+    fire.Fire(FindUnknownPeptides)
