afb32b5a750855671b4c26530fa5589930abdd18
omer <omermoses17@gmail.com>
2021-09-02 08:02:35 +0300

    add training prints

--- EpitopeWorkshop/cnn/train.py
+++ EpitopeWorkshop/cnn/train.py
@@ -1,18 +1,26 @@
 import torch
 import time
+import logging
 
 from torch.utils import data
 from EpitopeWorkshop.common.conf import DEFAULT_EPOCHS
 from EpitopeWorkshop.cnn.cnn import CNN
 
+log_format = "%(asctime)s : %(threadName)s: %(levelname)s : %(name)s : %(module)s : %(message)s"
+logging.basicConfig(format=log_format, level=logging.DEBUG)
+
 TEST_BATCH_SIZE = 20
 BATCH_SIZE = 4
 
 
-def train_model(model: 'CNN', dl_train: data.Dataset, epoch_amt: int = DEFAULT_EPOCHS):
+def train_model(model: 'CNN', dl_train: data.Dataset, dl_test: data.Dataset, epoch_amt: int = DEFAULT_EPOCHS):
+    test_accuracy, test_loss = [], []
+    train_accuracy, train_loss = [], []
+
     for epoch in range(epoch_amt):  # loop over the dataset multiple times
-        print(f"running for epoch {epoch + 1}")
-        running_loss = 0.0
+        logging.debug(f"running for epoch {epoch + 1}")
+        total_loss, n_correct = 0.0, 0.0
+        start_timestamp = time.time()
         for i, data in enumerate(dl_train):
             # get the inputs; data is a list of [inputs, labels]
             inputs, labels = data
@@ -26,41 +34,11 @@ def train_model(model: 'CNN', dl_train: data.Dataset, epoch_amt: int = DEFAULT_E
             loss.backward()
             model.optimizer.step()
 
-            # print statistics
-            # running_loss += loss.item()
-            if i % 2000 == 1999:  # print every 2000 mini-batches
-                print('[%d, %5d] loss: %.3f' %
-                      (epoch + 1, i + 1, running_loss / 2000))
-                running_loss = 0.0
-
-
-def train(model: 'CNN', dl_train: data.Dataset, dl_test: data.Dataset, epoch_amt: int = DEFAULT_EPOCHS, max_batches=20):
-    test_accuercies, test_losses = [], []
-    train_accuracy, train_loss = [], []
-
-    for epoch_idx in range(DEFAULT_EPOCHS):
-        total_loss, n_correct = 0, 0
-        start_timestamp = time.time()
-
-        for total_batch_idx, batch in enumerate(dl_train):
-            X, y = batch[0], batch[1]
-
-            # Forward pass
-            y_pred_log_proba = model(X)
-
-            # Backward pass
-            model.optimizer.zero_grad()
-            loss = model.criterion(y_pred_log_proba, y)
-            loss.backward()
-
-            # Weight updates
-            model.optimizer.step()
-
             # Calculate accuracy
             total_loss += loss.item()
-            y_pred = torch.argmax(y_pred_log_proba, dim=1)
-            n_correct += torch.sum(y_pred == y).float().item()
-            if (total_batch_idx + 1) % TEST_BATCH_SIZE == 0:
+            y_pred = torch.argmax(outputs, dim=1)
+            n_correct += torch.sum(y_pred == labels).float().item()
+            if (i + 1) % TEST_BATCH_SIZE == 0:
                 dl_test_iter = iter(dl_test)
                 test_total_acc, test_total_loss = 0, 0
                 for test_batch in dl_test_iter:
@@ -70,11 +48,12 @@ def train(model: 'CNN', dl_train: data.Dataset, dl_test: data.Dataset, epoch_amt
                     loss = model.criterion(test_pred_log_proba, test_y)
                     test_total_loss += loss.item()
                     test_total_acc += torch.sum(test_predication == test_y).float().item()
-                test_accuercies.append(test_total_acc / (len(dl_test) * BATCH_SIZE))
-                test_losses.append(test_total_loss / len(dl_test))
+                test_accuracy.append(test_total_acc / (len(dl_test) * BATCH_SIZE))
+                test_loss.append(test_total_loss / len(dl_test))
                 train_accuracy.append(n_correct / (TEST_BATCH_SIZE * BATCH_SIZE))
                 train_loss.append(total_loss / TEST_BATCH_SIZE)
-        print(
-            f"Epoch #{epoch_idx}, loss={total_loss / (max_batches):.3f}, accuracy={n_correct / (max_batches * BATCH_SIZE):.3f},elapsed={time.time() - start_timestamp:.1f} sec")
+        logging.debug(
+            f"Epoch #{epoch}, loss={total_loss / (TEST_BATCH_SIZE):.3f}, accuracy={n_correct / (TEST_BATCH_SIZE * BATCH_SIZE):.3f},elapsed={time.time() - start_timestamp:.1f} sec")
 
-    return train_accuracy, train_loss, test_accuercies, test_losses
+    return train_accuracy, train_loss, test_accuracy, test_loss
+
