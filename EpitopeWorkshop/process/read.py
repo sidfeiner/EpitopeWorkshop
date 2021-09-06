0c9860fa0efca3b998ceff497bd525c8dfff254c
Sid Feiner <sfeiner@vmware.com>
2021-09-07 00:51:41 +0300

    read data per sequence, calculate proba for input sequence

--- EpitopeWorkshop/process/read.py
+++ EpitopeWorkshop/process/read.py
@@ -1,4 +1,4 @@
-from typing import List, Optional, Set
+from typing import List, Optional, Set, Tuple
 
 from Bio import SeqIO
 import pandas as pd
@@ -57,7 +57,44 @@ def load_fasta_row_per_aa(path: str, with_sliding_window: bool = True,
     return pd.DataFrame(data)
 
 
-def load_fasta_row_per_window(path: str, with_sliding_window: bool = True,
+def build_df(ids, seqs, sub_seqs, sub_seq_index_start, is_in_epitope):
+    data = {ID_COL_NAME: ids, SEQ_COL_NAME: seqs, SUB_SEQ_INDEX_START_COL_NAME: sub_seq_index_start,
+            SUB_SEQ_COL_NAME: sub_seqs, IS_IN_EPITOPE_COL_NAME: is_in_epitope}
+    return pd.DataFrame(data)
+
+
+def prep_data_per_sequence(
+        seq: str, seq_id: Optional[str] = None,
+        sliding_window_size: int = DEFAULT_WINDOW_SIZE
+) -> Tuple[List[Optional[str]], List[str], List[str], List[int], List[int]]:
+    ids = []
+    seqs = []
+    sub_seqs = []  # type: List[str]
+    sub_sequence_index_start = []
+    is_in_epitope = []  # type: List[int]
+
+    current_sub_seqs = utils.split_to_subsequences(
+        seq,
+        sliding_window_size,
+        conf.NO_AMINO_ACID_CHAR
+    )
+    assert len(current_sub_seqs) == len(seq)
+    mult_basic_values = len(seq)
+    ids.extend(mult_basic_values * [seq_id])
+    seqs.extend(mult_basic_values * [seq])
+    sub_seqs.extend(current_sub_seqs)
+    sub_sequence_index_start.extend(range(-sliding_window_size // 2 + 1, len(seq) - sliding_window_size // 2))
+
+    is_in_epitope.extend(
+        (
+            0 if sub_seq[len(sub_seq) // 2] == conf.NO_AMINO_ACID_CHAR
+            else int(sub_seq[len(sub_seq) // 2].isupper()) for sub_seq in current_sub_seqs
+        )
+    )
+    return ids, seqs, sub_seqs, sub_sequence_index_start, is_in_epitope
+
+
+def load_fasta_row_per_window(path: str,
                               sliding_window_size: int = DEFAULT_WINDOW_SIZE,
                               limit_sequences_amt: Optional[int] = None
                               ) -> pd.DataFrame:
@@ -68,33 +105,15 @@ def load_fasta_row_per_window(path: str, with_sliding_window: bool = True,
     is_in_epitope = []  # type: List[int]
     with open(path) as handle:
         for idx, record in enumerate(SeqIO.parse(handle, "fasta")):
-            current_sub_seqs = utils.split_to_subsequences(
-                record.seq,
-                sliding_window_size,
-                conf.NO_AMINO_ACID_CHAR
-            ) if with_sliding_window else [record.seq]
-            if with_sliding_window:
-                assert len(current_sub_seqs) == (
-                        len(record.seq) - sliding_window_size + 1 + 2 * (sliding_window_size - 1))
-            mult_basic_values = len(current_sub_seqs) if with_sliding_window else len(record.seq)
-            ids.extend(mult_basic_values * [record.id])
-            seqs.extend(mult_basic_values * [record.seq])
-            sub_seqs.extend(current_sub_seqs)
-            if with_sliding_window:
-                sub_sequence_index_start.extend(range(-sliding_window_size +1, 0))
-            starting_indexes = range(len(record.seq)) if with_sliding_window else range(len(current_sub_seqs))
-            sub_sequence_index_start.extend(starting_indexes)
-            is_in_epitope.extend(
-                (
-                    0 if sub_seq[len(sub_seq) // 2] == conf.NO_AMINO_ACID_CHAR
-                    else int(sub_seq[len(sub_seq) // 2].isupper()) for sub_seq in current_sub_seqs
-                )
-            )
+            cur_ids, cur_seqs, cur_sub_seqs, cur_sub_seq_index_start, cur_is_in_epitope = \
+                prep_data_per_sequence(record.seq, record.id, sliding_window_size)
+            ids.extend(cur_ids)
+            seqs.extend(cur_seqs)
+            sub_seqs.extend(cur_sub_seqs)
+            sub_sequence_index_start.extend(cur_sub_seq_index_start)
+            is_in_epitope.extend(cur_is_in_epitope)
             if len(record.features):
                 print(f"found features: {record.features}")
             if limit_sequences_amt is not None and limit_sequences_amt == idx:
                 break
-
-    data = {ID_COL_NAME: ids, SEQ_COL_NAME: seqs, SUB_SEQ_INDEX_START_COL_NAME: sub_sequence_index_start,
-            SUB_SEQ_COL_NAME: sub_seqs, IS_IN_EPITOPE_COL_NAME: is_in_epitope, }
-    return pd.DataFrame(data)
+    return build_df(ids, seqs, sub_sequence_index_start, sub_seqs, is_in_epitope)
--- run_module.py
+++ run_module.py
@@ -4,76 +4,50 @@ import pandas as pd
 import numpy as np
 import torch
 
-import torch.utils.data as data_utils
 from EpitopeWorkshop.common.conf import DEFAULT_USING_NET_DEVICE, PATH_TO_CNN, DEFAULT_IS_IN_EPITOPE_THRESHOLD
 from EpitopeWorkshop.cnn.cnn import CNN
 from EpitopeWorkshop.dataset.EpitopeDataset import EpitopeDataset
 from EpitopeWorkshop.common import contract, utils, conf
 from EpitopeWorkshop.process.features import FeatureCalculator
-from EpitopeWorkshop.process.read import load_fasta_row_per_window, load_fasta_row_per_aa
-from EpitopeWorkshop.scripts.calculate_features import FileFeatureCalculator
+from EpitopeWorkshop.process.read import load_fasta_row_per_window, prep_data_per_sequence, build_df
 
 log_format = "%(asctime)s : %(threadName)s: %(levelname)s : %(name)s : %(module)s : %(message)s"
 logging.basicConfig(format=log_format, level=logging.DEBUG)
 
-def is_upper(prop: torch.float32) -> bool:
-    return prop>= IS_IN_EPITOP_THRESHOLD
 
+class ClassifyPeptide:
 
-def load_net(path: str = PATH_TO_CNN):
-    cnn = CNN()
-    cnn.load_state_dict(torch.load(path, map_location=DEFAULT_USING_NET_DEVICE))
-    cnn.eval()
+    def load_net(self, path: str = PATH_TO_CNN):
+        cnn = CNN()
+        cnn.load_state_dict(torch.load(path, map_location=DEFAULT_USING_NET_DEVICE))
+        cnn.eval()
 
-    return cnn
+        return cnn
 
-def prepare_data(prot: str) -> EpitopeDataset:
-    df = load_fasta_row_per_window(path='C:\\Users\\User\\OneDrive\\TAU\\cs\\EpitopeWorkshop\\iedb_linear_epitopes_partial.fasta')
-    calculator = FeatureCalculator()
-    data = calculator.calculate_features(df)
-    calculated_features = data[contract.CALCULATED_FEATURES_COL_NAME]
-    value = [np.array(ten) for ten in calculated_features]
-    val = torch.tensor(value, dtype=torch.float32)
+    def prepare_data(self, seq: str):
+        data = prep_data_per_sequence(seq)
+        df = build_df(*data)
+        calculator = FeatureCalculator()
+        df = calculator.calculate_features(df)
+        values = df[contract.CALCULATED_FEATURES_COL_NAME].to_numpy()
+        return torch.tensor(np.stack(values))
 
-    return val
+    def classify_peptide(self, peptide: str, threshold: float = DEFAULT_IS_IN_EPITOPE_THRESHOLD,
+                         device: str = DEFAULT_USING_NET_DEVICE):
+        logging.info(f"preparing data to input")
+        data = self.prepare_data(peptide)
 
+        logging.info(f"activating trained CNN")
+        cnn = self.load_net()
 
-def creat_predication(predications: torch.tensor, letters: pd.Series) -> pd.Series:
-    start_seq_index = 0
-    end_seq_index = 0
-    for i, letter in enumerate(predications):
-        if letter >= DEFAULT_IS_IN_EPITOPE_THRESHOLD:
-            if countRowCapital == 0:
-                firstCapialIndex = i
-            countRowCapital += 1
-        else:
-            if countRowCapital < conf.MIN_EPITOP_SIZE:
-                for x in range(firstCapialIndex, i + 1):
-                    finalPredictedSeq[x] = finalPredictedSeq[x].lower()
-            firstCapialIndex = i + 1
-            countRowCapital = 0
+        logging.info(f"running.....")
+        epitope_probas = torch.sigmoid(cnn(data))
 
-def some_name(input_by_user: str, device: str = DEFAULT_USING_NET_DEVICE):
+        logging.info(f"finished calculating probabilities, creating predication")
+        epitopes_classification = epitope_probas >= threshold
 
-    letters_data = pd.Series([letter for letter in input_by_user], dtype="string")
-    #letters_data.apply(is_upper)
-    logging.info(f"preparing data to input")
-    data = prepare_data(input_by_user)
+        print("Done")
 
-    logging.info(f"activating trained CNN")
-    cnn = load_net()
-
-    logging.info(f"running.....")
-    outputs = pd.DataFrame(cnn(data)).astype("float")
-    outputs.apply(is_upper)
-
-
-    logging.info(f"finished calculating probabilities, creating predication")
-
-
-
-
-    print("Done")
 
 if __name__ == '__main__':
-    fire.Fire(some_name)
+    fire.Fire(ClassifyPeptide)
